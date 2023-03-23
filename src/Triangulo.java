e3a952eb725fd3db66821921d33a597733e2eccf
ggarcia1511 <gye120194@gmail.com>
2023-03-22 18:07:49 -0600

    Calculando el Area Triangulo 2

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -1,7 +1,7 @@
 public class Triangulo {
 public void calcularTriangulo(int b, int h){
     System.out.println("Calculando el area de un Triangulo");
-    int calcular = b * h;
+    int calcular = (b * h) / 2;
     System.out.println("El area es: " + calcular);
 
 }
