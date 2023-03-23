aa44a94f7c2a3e1aadcb87990ad145e6a585bec4
ggarcia1511 <gye120194@gmail.com>
2023-03-23 16:08:25 -0600

    Triangulo

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -1,2 +1,12 @@
 public class Triangulo {
+    private Integer b;
+    private Integer h;
+    public Triangulo (Integer b, Integer h){
+        this.b = b;
+        this.h= h;
+    }
+
+    public Integer calcular (){
+        return  (b*h)/2;
+    }
 }
